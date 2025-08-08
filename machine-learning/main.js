c383e57028f0a0e967b05f16ffd54437bcc99cb0
Erick Wendel <erick.workspace@gmail.com>
2025-08-08 16:20:42 -0300

    fixing performance
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- machine-learning/main.js
+++ machine-learning/main.js
@@ -9,31 +9,30 @@ export default async function main(game) {
     worker.onmessage = ({ data }) => {
         const { type, x, y } = data;
 
-        if (type !== 'prediction') return
+        if (type === 'prediction') {
+            console.log(`🎯 AI predicted at: (${x}, ${y})`);
+            container.updateHUD(data);
+            game.stage.aim.visible = true;
 
-        container.updateHUD(data);
-        game.stage.aim.visible = true;
+            game.stage.aim.setPosition(data.x, data.y);
+            const position = game.stage.aim.getGlobalPosition();
 
-        game.stage.aim.setPosition(x, y);
-        const position = game.stage.aim.getGlobalPosition();
+            game.handleClick({
+                global: position,
+            });
 
-        game.handleClick({
-            global: position,
-        });
+        }
+
     };
 
-    setInterval(() => {
+    setInterval(async () => {
         const canvas = game.app.renderer.extract.canvas(game.stage);
-
-        const ctx = canvas.getContext('2d');
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const bitmap = await createImageBitmap(canvas);
 
         worker.postMessage({
             type: 'predict',
-            buffer: imageData.data.buffer,
-            width: imageData.width,
-            height: imageData.height
-        }, [imageData.data.buffer]);
+            image: bitmap,
+        }, [bitmap]);
 
     }, 200); // every 200ms
 
